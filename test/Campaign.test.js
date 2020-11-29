3971dd9a755ca66ce01348802a725e00cd293295
thurendous <thurendous2@gmail.com>
2020-11-30 00:40:03 +0900

    ETH side finished 11/30

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -38,4 +38,66 @@ describe("Campaigns", () => {
     assert.ok(factory.options.address);
     assert.ok(campaign.options.address);
   });
+
+  it("marks caller as the campaign manager", async () => {
+    const manager = await campaign.methods.manager().call();
+
+    assert.equal(accounts[0], manager);
+  });
+
+  it("alows people to contribute money, and marks them as approvers.", async () => {
+    await campaign.methods.contribute().send({
+      value: 200,
+      from: accounts[1],
+    });
+    const isContributor = await campaign.methods.approvers(accounts[1]).call(); // use the approvers function in the contract
+    assert(isContributor);
+  });
+
+  it("requires a minimum contribution", async () => {
+    try {
+      await campaign.methods.contribute().send({
+        value: "5",
+        from: accounts[1],
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it("allows a manager to make a payment request", async () => {
+    await campaign.methods
+      .createRequest("Buy Batteries", "100", accounts[1])
+      .send({ from: accounts[0], gas: "1000000" });
+    const request = await campaign.methods.requests(0).call();
+    assert.equal("Buy Batteries", request.description);
+  });
+
+  it("processes the request", async () => {
+    await campaign.methods.contribute().send({
+      from: accounts[0],
+      value: web3.utils.toWei("10", "ether"),
+    });
+    await campaign.methods
+      .createRequest("A", web3.utils.toWei("5", "ether"), accounts[1])
+      .send({
+        from: accounts[0],
+        gas: "1000000",
+      });
+    await campaign.methods.approveRequest(0).send({
+      from: accounts[0],
+      gas: "1000000",
+    });
+
+    await campaign.methods.finalizeRequest(0).send({
+      from: accounts[0],
+      gas: "1000000",
+    });
+    let balance = await web3.eth.getBalance(accounts[1]);
+    balance = web3.utils.fromWei(balance, "ether");
+    balance = parseFloat(balance); // change string into a decimal num
+    console.log(balance);
+    assert(balance > 104);
+  });
 });
